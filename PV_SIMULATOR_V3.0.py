e4e7bcfed0b3d1320f2dad6a21fa6d88e7b0205f
xia.mei <meixia0731@gmail.com>
2022-08-07 07:27:11 +1000

    unpack(H)

--- PV_SIMULATOR_V3.0.py
+++ PV_SIMULATOR_V3.0.py
@@ -1,96 +1,101 @@
-import threading
-import modbus_tk
-import modbus_tk.defines as cst
-import modbus_tk.modbus as modbus
-import modbus_tk.modbus_tcp as modbus_tcp
-import time
 import random
 import struct
+import time
 
-#--------------------------------------------------------------------t 
+import modbus_tk.defines as cst
+import modbus_tk.modbus_tcp as modbus_tcp
+
+# --------------------------------------------------------------------t
 modbus_slave_ip = '0.0.0.0'
-#Listening port
+# Listening port
 modbus_slave_port = 502
 
-#Listening slave ID
+# Listening slave ID
 modbus_slave_id = 1
 
-
-Active_Power_Scaling = 1
-Active_Power_SP_Scaling = 1
-
+active_power_scaling = 1
+active_power_sp_scaling = 1
 
 # list[address,length,sign,default_value]
-Active_Power_addr= [8069,'uint64',65536]
-Reactive_Power_addr = [8075,'int64',10000]
-Limitation_Power_addr = [8085,32,2,30000]
-Start_Stop_Status_addr = [8067,16,2,21]
-Start_Stop_Cmd_addr = [8002,16,2,1]
-Active_Power_SP_addr = [8005,32,2,30000]
-Energy_addr = [8079,64,2,0]
+active_power_addr = [8069, 'uint64', 30000]
+reactive_power_addr = [8075, 'int64', 10000]
+limitation_power_addr = [8085, 32, 2, 30000]
+start_stop_status_addr = [8067, 16, 2, 21]
+start_stop_cmd_addr = [8002, 16, 2, 1]
+active_power_sp_addr = [8005, 32, 2, 30000]
+energy_addr = [8079, 64, 2, 0]
 
 # Ramp_rate: 1 means percentage, 2 means kw
 Ramp_rate_type = 1
 Ramp_rate_value = 5
 Ramp_rate_percentage = 0.3
 
-#------------------------------------------------------------------------------
 
-def PV_SIMULATOR():
-    #Create the server
-    server = modbus_tcp.TcpServer(address=modbus_slave_ip, port=modbus_slave_port)    
+# ------------------------------------------------------------------------------
+
+def pv_simulator():
+    # Create the server
+    server = modbus_tcp.TcpServer(address=modbus_slave_ip, port=modbus_slave_port)
     server.start()
-    
-    #Add slave
+
+    # Add slave
     slave_1 = server.add_slave(modbus_slave_id)
-    
-    #Add data blocks
+
+    # Add data blocks
     slave_1.add_block('A', cst.ANALOG_INPUTS, 8000, 150)
     slave_1.add_block('B', cst.HOLDING_REGISTERS, 8000, 10)
-    
-    #Initialization, convert data to machine code  
-    Active_Power_C = int2C(Active_Power_addr[1],Active_Power_addr[2])
-    print(Active_Power_C)
-    slave_1.set_values('A', Active_Power_addr[0], Active_Power_C)
 
-    #Read sepoint and generate feedback    
+    # Initialization, convert data to machine code
+    active_power_c = int2C(active_power_addr[1], active_power_addr[2])
+    slave_1.set_values('A', active_power_addr[0], active_power_c)
+
+    # Read setpoint and generate feedback
     while True:
         # Read data from memory, convert it from machine code to int. These values are input of the simulator engine
-        Active_Power_C = slave_1.get_values('A',Active_Power_addr[0],4)
-        print(Active_Power_C)
-        Active_Power_INT = C2int(Active_Power_addr[1],Active_Power_C)
-        print(Active_Power_INT)        
-
-        Active_Power_INT = random.randrange(1,999999999999)
-        print(Active_Power_INT)
-        Active_Power_C = int2C(Active_Power_addr[1],Active_Power_INT)
-        print(Active_Power_C)
-        slave_1.set_values('A', Active_Power_addr[0], Active_Power_C)
+        active_power_c = slave_1.get_values('A', active_power_addr[0], 4)
+        print('read:active_power_c',active_power_c)
+        active_power_int = C2int(active_power_addr[1], active_power_c)
+        print('decode:active_power_int',active_power_int)
+        active_power_int = random.randrange(18446744073709551615)
+        print('new:active_power_int',active_power_int)
+        active_power_c = int2C(active_power_addr[1], active_power_int)
+        print('send:active_power_c',active_power_c)
+        slave_1.set_values('A', active_power_addr[0], active_power_c)
 
         time.sleep(2)
         print('--------------------------------')
-        #if stop command received, change P setpoint to zero. New P = P + (P_setpint-P)*Ramprate
- #       if Start_Stop_Cmd_INT == 0:
- #           Active_Power_INT =  int(((0 - Active_Power_INT)*Ramp_rate_percentage + Active_Power_INT)*random.uniform(0.98,1.02)/Active_Power_Scaling)
- #       else:
- #           Active_Power_INT =  int(((min(Active_Power_SP_INT,Limitation_Power_INT) - Active_Power_INT)*Ramp_rate_percentage + Active_Power_INT)*random.uniform(0.99,1.01)/Active_Power_Scaling)
+        # if stop command received, change P setpoint to zero. New P = P + (P_setpint-P)*Ramprate
 
-        # Converter P to machine code
- #       Active_Power_MC = Converter(Active_Power_INT,Active_Power_addr[1],1,Active_Power_addr[2])
-        # Update P to memory
- #       slave_1.set_values('A', Active_Power_addr[0], Active_Power_MC)
- #       print('Sent out Active_Power is', Active_Power_INT, 'kW','Scaling:',Active_Power_Scaling)
 
-def int2C(data_type,value):
+#       if Start_Stop_Cmd_INT == 0:
+#           Active_Power_INT =  int(((0 - Active_Power_INT)*Ramp_rate_percentage + Active_Power_INT)*random.uniform(0.98,1.02)/active_power_scaling)
+#       else:
+#           Active_Power_INT =  int(((min(Active_Power_SP_INT,Limitation_Power_INT) - Active_Power_INT)*Ramp_rate_percentage + Active_Power_INT)*random.uniform(0.99,1.01)/active_power_scaling)
+
+# Converter P to machine code
+#       Active_Power_MC = Converter(Active_Power_INT,active_power_addr[1],1,active_power_addr[2])
+# Update P to memory
+#       slave_1.set_values('A', active_power_addr[0], Active_Power_MC)
+#       print('Sent out Active_Power is', Active_Power_INT, 'kW','Scaling:',active_power_scaling)
+
+def int2C(data_type, value):
     if data_type == 'uint64':
-        value = struct.pack('>Q',value)
-        return[value[0]*16+value[1],value[2]*16+value[3],value[4]*16+value[5],value[6]*16+value[7]]
-
-def C2int(data_type,value):
+        value = struct.pack('>Q', value)
+        return [struct.unpack('>H',value[0:2])[0], struct.unpack('>H',value[2:4])[0], struct.unpack('>H',value[4:6])[0], struct.unpack('>H',value[6:8])[0]]
+    if data_type == 'int64':
+        value = struct.pack('>q', value)
+        return [value[0] * 16 + value[1], value[2] * 16 + value[3], value[4] * 16 + value[5], value[6] * 16 + value[7]]
+def C2int(data_type, value):
+    bytes_value = b''
     if data_type == 'uint64':
-        bytes_value = struct.pack('>H',value[0]) + struct.pack('>H',value[1]) + struct.pack('>H',value[2]) + struct.pack('>H',value[3])
-        value = struct.unpack('>Q',bytes_value)
+        for i in value:
+            bytes_value = bytes_value + struct.pack('>H',i)
+#        print('C2int output', struct.unpack('>Q',bytes_value))
+        return struct.unpack('>Q',bytes_value)[0]
+    if data_type == 'int64':
+        bytes_value = struct.pack('>H', value[0]) + struct.pack('>H', value[1]) + struct.pack('>H', value[2]) + struct.pack('>H', value[3])
+        value = struct.unpack('>q', bytes_value)
         return value
 
 if __name__ == "__main__":
-    PV_SIMULATOR()
+    pv_simulator()
